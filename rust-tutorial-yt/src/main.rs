9d0449e13834886cb42097cba9981ded2a203683
Kanishk Pachauri <itskanishkp.py@gmail.com>
2025-03-19 02:16:38 +0530

    feat: learning rust

--- rust-tutorial-yt/src/main.rs
+++ rust-tutorial-yt/src/main.rs
@@ -2,31 +2,95 @@
 
 // use rand::Rng; used for gussing_game
 
+
 // This file contains the examples of rustbook.
 fn main() {
     println!("Hello, world!"); // Hello, world!
 
-    data_types();
+
+
+    // data_types();
 
     // variables_and_mutability();
 
     // guessing_game();
 }
 
-fn data_types() {
-    /*
-        Scalar Types - represents only a single value
-        - int
-        - float
-        - bool
-        - char
-    */
 
-    for number in (1..4).rev() {
-        println!("{number}!");
-    }
-    println!("LIFTOFF!!!");
-}
+// #[allow(unused)]
+// fn data_types() {
+//     /*
+//         Scalar Types - represents only a single value
+//         - int
+//         - float
+//         - bool
+//         - char
+//     */
+
+//     // integer
+//     let my_number = 100_000; // default i32
+    
+//     let this_size_by_system: usize = 120_000;
+
+
+//     // integer overflow
+//     let a_u8_int: u8 = 255u8;
+//     // wrapping_* Methods
+//     let y = a_u8_int.wrapping_add(1); // y = 0 ie. 266 = 0 267 = 1.... 
+
+//     // checked_* Methods
+
+//     // floating point
+//     let f = 3.15; // default f64
+
+//     // boolean
+//     let t = true;
+//     let f: bool = false;
+
+//     let character: char = 'a';
+
+//     // tuple
+//     /*
+//         - have fixed length ( can't grow or shrink )
+//         - have predefined types and can be a consit of any type
+//     */ 
+
+//     let tup = (1, "Hello", 3.14, true);
+
+//     let tupa: (i32, u8, bool, f64, bool, &str) = (123, 255, true, 3.145, false, "World");
+
+//     let (x, y, z, a) = tup;
+
+//     let pi = tup.3;
+
+//     println!("{}", tup.0); // prints what at tup[0] index
+
+//     // array type
+//     /*
+//         - fixed length ( can't grow or shrink )
+//         - contains elements of same type
+//         - store data in the stack
+//     */
+
+//     let arr = [1,2,3,4,5];
+//     let my_array: [i32; 5] = [2, 4, 6, 8, 10];
+
+//     let months = ["January", "February", "March", "April", "May", "June", "July",
+//               "August", "September", "October", "November", "December"];
+
+//     let copy = ['a'; 5]; // result ['a', 'a', 'a', 'a', 'a']
+
+//     // accessing he elment of an arrya
+//     let first_month = months[0];
+
+//     let last_moth = months[months.len() - 1]; // 12 - 1
+    
+//     // let out_of_bound_length = months[13];
+
+
+
+
+// }
 
 // fn variables_and_mutability() {
 //     const THREE_HOURS_IN_SECONDS: u32 = 3 * 60 * 60;
